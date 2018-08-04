da2cb1bf6921feef007b71a1515fd87d4ef5f385
Mehul <mehul.brahmvar@hotmail.com>
2018-08-04 10:24:58 +0530

    Redirect from one activity to other

---
+++ app/src/main/java/journaler/com/learnkotlin/DisplayToastActivity.kt
@@ -0,0 +1,12 @@
+package journaler.com.learnkotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DisplayToastActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display_toast)
+    }
+}
--- app/src/main/java/journaler/com/learnkotlin/MainActivity.kt
+++ app/src/main/java/journaler/com/learnkotlin/MainActivity.kt
@@ -1,9 +1,11 @@
 package journaler.com.learnkotlin
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import journaler.com.learnkotlin.R.layout.activity_display_toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +17,9 @@ class MainActivity : AppCompatActivity() {
         btnShowToast.setOnClickListener(){
             Log.i("MainActivity", "btnShowToast clicked")
             Toast.makeText(this, "Button was clicked", Toast.LENGTH_LONG).show()
+
+            var intent = Intent(this, DisplayToastActivity:: class.java)
+            startActivity(intent)
         }
     }
 }
